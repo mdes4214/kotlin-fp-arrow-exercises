6fa48dca854c1f4b2407c0136c9ddd45189c7825
Jack Lo <mdes4214@gmail.com>
2021-10-06 23:03:56 +0800

    update solution of DeepCopy in ch3 exercises

--- src/main/kotlin/ch3_optics/ex1_deepcopy/DeepCopy.kt
+++ src/main/kotlin/ch3_optics/ex1_deepcopy/DeepCopy.kt
@@ -1,5 +1,7 @@
 package ch3_optics.ex1_deepcopy
 
+import arrow.optics.optics
+
 @JvmInline value class Content(val value: String)
 @JvmInline value class FileExtension(val value: String)
 @JvmInline value class FileName(val value: String)
@@ -17,30 +19,34 @@ enum class Tag {
 // This test will pass initially, but we want to refactor it with `@optics`.
 // *** TODO section START ***
 
+@optics
 data class MyFile(
     val header: MyHeader,
     val content: Content,
     val extension: FileExtension,
     val name: FileName,
-)
+) {
+    companion object
+}
 
+@optics
 data class MyHeader(
     val metadata: MyMetadata
-)
+) {
+    companion object
+}
 
+@optics
 data class MyMetadata(
     val tag: Tag,
     val title: Title,
     val author: Author
-)
+) {
+    companion object
+}
 
 fun MyFile.updateTag(newTag: Tag): MyFile =
-    this.copy(
-        header = this.header.copy(
-            metadata = this.header.metadata.copy(
-                tag = newTag
-            )
-        )
-    )
+    MyFile.header.metadata.tag.set(this, newTag)
+
 
 // *** TODO section END ***

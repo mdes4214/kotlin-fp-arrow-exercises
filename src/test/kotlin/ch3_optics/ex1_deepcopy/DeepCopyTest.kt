9178791d1011cf9a43aaa3ab430a814ddef7b0b8
Jack Lo <mdes4214@gmail.com>
2021-10-06 22:58:52 +0800

    init pom.xml, add DeepCopy in ch3 exercises

---
+++ src/test/kotlin/ch3_optics/ex1_deepcopy/DeepCopyTest.kt
@@ -0,0 +1,24 @@
+package ch3_optics.ex1_deepcopy
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class DeepCopyTest {
+    @Test
+    fun testUpdateTag() {
+        val docFile = MyFile(
+            header = MyHeader(
+                metadata = MyMetadata(
+                    tag = Tag.TYPE_C,
+                    title = Title("Functional Programming Learning Note"),
+                    author = Author("Jack")
+                )
+            ),
+            content = Content("This is a note for Functional Programming learning."),
+            extension = FileExtension(".doc"),
+            name = FileName("FP_note")
+        )
+
+        Assertions.assertEquals(Tag.TYPE_A, docFile.updateTag(Tag.TYPE_A).header.metadata.tag, "The tag is not updated correctly.")
+    }
+}
